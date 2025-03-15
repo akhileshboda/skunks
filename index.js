86ff14c8f87c28ad5dcde4f303d437f9709f4d03
Akhil Boda <akboda988@gmail.com>
2025-03-15 20:55:22 +1100

    updated settings window popup functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// First load the navigation
+/**
+ * Function to load the nav bar and listen for when the settings button is clicked
+ */
 document.addEventListener('DOMContentLoaded', function() {
     // Load the navigation
     fetch('nav.html')
@@ -11,12 +13,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Prevent the default link behavior
                 e.preventDefault();
 
+                // Get screen dimension
+                const screenWidth = window.screen.availWidth;
+                const screenHeight = window.screen.availHeight;
+
+                // Settings window dimensions
+                const windowWidth = 400;
+                const windowHeight = 500;
+
+                // Calculate center screen position
+                const x_axis = Math.round(screenWidth - windowWidth) / 2;
+                const y_axis = Math.round(screenHeight - windowHeight) / 2;
+
                 // Open settings in a popup window
                 chrome.windows.create({
                     url: chrome.runtime.getURL('settings.html'),
                     type: 'popup',
-                    width: 400,
-                    height: 500
+                    width: windowWidth,
+                    height: windowHeight,
+                    left: x_axis,
+                    top: y_axis
                 });
             });
         });
