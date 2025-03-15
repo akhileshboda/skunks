1ff49dcd4d8d4fca227e9da007b809089942dd61
SophiaQ-cpu <squa0021@student.monash.edu>
2025-03-15 23:32:22 +1100

    - Made it so that the settings popup window automatically opens on the center of the screen.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             document.getElementById('nav-import').innerHTML = data;
 
+
+
             // Now that the nav is loaded, add the event listener
             document.getElementById('openSettings').addEventListener('click', function(e) {
                 // Prevent the default link behavior
@@ -16,7 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     url: chrome.runtime.getURL('settings.html'),
                     type: 'popup',
                     width: 700,
-                    height: 500
+                    height: 500,
+                    left: Math.round((window.screen.availWidth - 700) / 2),
+                    top:  Math.round((window.screen.availHeight - 500) / 2)
                 });
             });
         });
