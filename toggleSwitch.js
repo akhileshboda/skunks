55cbe9d81433ab921c030214909977b4e670b927
ruby-hall <80376265+ruby-hall@users.noreply.github.com>
2025-03-16 14:55:29 +1100

    Create toggleSwitch.js

---
+++ toggleSwitch.js
@@ -0,0 +1,29 @@
+let toggle = document.getElementById("toggle");
+
+function changeDisplay(isChecked){
+    let searchDiv = document.getElementById("searchDiv");
+    console.log(searchDiv.innerHTML);
+    console.log(searchDiv.style.display);
+    if(isChecked){
+        searchDiv.style.display = "block";
+    }
+    else {
+        searchDiv.style.display = "none";
+    }
+}
+
+toggle.addEventListener("change", function(){
+    console.log(toggle.checked);
+    chrome.storage.local.set({"isChecked": toggle.checked});
+    changeDisplay(toggle.checked);
+});
+
+chrome.storage.local.get("isChecked", function(result){
+    if (result){
+        isChecked = result.isChecked;
+        toggle.checked = isChecked;
+        changeDisplay(isChecked);
+    }
+})
+
+//console.log(searchDiv.innerHTML);
