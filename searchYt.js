eb9195cc4336e98bf9fad236cfe7ebdc1c705182
ruby-hall <80376265+ruby-hall@users.noreply.github.com>
2025-03-16 12:17:22 +1100

    Create searchYt.js
    havent been implemented yet - pending

---
+++ searchYt.js
@@ -0,0 +1,35 @@
+// import "https://apis.google.com/js/api.js";
+
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyAq3E-BBZXMm1w69fra9hPNYRxMDbGQTfs");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 25,
+      "q": "surfing"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "YO432561064730-adfal5if7acni9daeef9bhkrkpa6230q.apps.googleusercontent.com"});
+  });
+
+  authenticate.then(loadClient);
+  execute();
